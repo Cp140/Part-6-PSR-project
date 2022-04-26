917d00f786c9f10e91a19836370d70cada5e12ec
Cole Palmer <PalmCole551@GoTVDSB.ca>
2022-04-26 10:54:48 -0400

    Worked On It!!!!!!!!!!!!!!!!!!!!!!!!!!

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -28,41 +28,41 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.pictureBox1 = new System.Windows.Forms.PictureBox();
-            this.pictureBox2 = new System.Windows.Forms.PictureBox();
+            this.img1 = new System.Windows.Forms.PictureBox();
+            this.img2 = new System.Windows.Forms.PictureBox();
             this.RBPaper = new System.Windows.Forms.RadioButton();
             this.BRScissors = new System.Windows.Forms.RadioButton();
             this.RBRock = new System.Windows.Forms.RadioButton();
             this.GBOne = new System.Windows.Forms.GroupBox();
-            this.button1 = new System.Windows.Forms.Button();
-            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
-            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
+            this.btnPlay = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.img1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.img2)).BeginInit();
             this.GBOne.SuspendLayout();
             this.SuspendLayout();
             // 
-            // pictureBox1
+            // img1
             // 
-            this.pictureBox1.Location = new System.Drawing.Point(12, 12);
-            this.pictureBox1.Name = "pictureBox1";
-            this.pictureBox1.Size = new System.Drawing.Size(301, 278);
-            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
-            this.pictureBox1.TabIndex = 0;
-            this.pictureBox1.TabStop = false;
+            this.img1.Location = new System.Drawing.Point(12, 12);
+            this.img1.Name = "img1";
+            this.img1.Size = new System.Drawing.Size(301, 278);
+            this.img1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.img1.TabIndex = 0;
+            this.img1.TabStop = false;
             // 
-            // pictureBox2
+            // img2
             // 
-            this.pictureBox2.Location = new System.Drawing.Point(487, 12);
-            this.pictureBox2.Name = "pictureBox2";
-            this.pictureBox2.Size = new System.Drawing.Size(301, 278);
-            this.pictureBox2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
-            this.pictureBox2.TabIndex = 1;
-            this.pictureBox2.TabStop = false;
+            this.img2.Location = new System.Drawing.Point(487, 12);
+            this.img2.Name = "img2";
+            this.img2.Size = new System.Drawing.Size(301, 278);
+            this.img2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.img2.TabIndex = 1;
+            this.img2.TabStop = false;
             // 
             // RBPaper
             // 
             this.RBPaper.AutoSize = true;
             this.RBPaper.Font = new System.Drawing.Font("Microsoft Sans Serif", 11F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.RBPaper.Location = new System.Drawing.Point(6, 16);
+            this.RBPaper.Location = new System.Drawing.Point(6, 47);
             this.RBPaper.Name = "RBPaper";
             this.RBPaper.Size = new System.Drawing.Size(65, 22);
             this.RBPaper.TabIndex = 2;
@@ -75,7 +75,7 @@
             // 
             this.BRScissors.AutoSize = true;
             this.BRScissors.Font = new System.Drawing.Font("Microsoft Sans Serif", 11F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.BRScissors.Location = new System.Drawing.Point(6, 39);
+            this.BRScissors.Location = new System.Drawing.Point(6, 19);
             this.BRScissors.Name = "BRScissors";
             this.BRScissors.Size = new System.Drawing.Size(85, 22);
             this.BRScissors.TabIndex = 3;
@@ -88,7 +88,7 @@
             // 
             this.RBRock.AutoSize = true;
             this.RBRock.Font = new System.Drawing.Font("Microsoft Sans Serif", 11F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.RBRock.Location = new System.Drawing.Point(6, 62);
+            this.RBRock.Location = new System.Drawing.Point(6, 75);
             this.RBRock.Name = "RBRock";
             this.RBRock.Size = new System.Drawing.Size(62, 22);
             this.RBRock.TabIndex = 4;
@@ -99,37 +99,39 @@
             // 
             // GBOne
             // 
-            this.GBOne.Controls.Add(this.BRScissors);
             this.GBOne.Controls.Add(this.RBRock);
             this.GBOne.Controls.Add(this.RBPaper);
+            this.GBOne.Controls.Add(this.BRScissors);
             this.GBOne.Location = new System.Drawing.Point(685, 296);
             this.GBOne.Name = "GBOne";
-            this.GBOne.Size = new System.Drawing.Size(103, 100);
+            this.GBOne.Size = new System.Drawing.Size(103, 101);
             this.GBOne.TabIndex = 5;
             this.GBOne.TabStop = false;
             // 
-            // button1
+            // btnPlay
             // 
-            this.button1.Location = new System.Drawing.Point(409, 374);
-            this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(75, 23);
-            this.button1.TabIndex = 6;
-            this.button1.Text = "button1";
-            this.button1.UseVisualStyleBackColor = true;
+            this.btnPlay.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnPlay.Location = new System.Drawing.Point(357, 212);
+            this.btnPlay.Name = "btnPlay";
+            this.btnPlay.Size = new System.Drawing.Size(90, 78);
+            this.btnPlay.TabIndex = 6;
+            this.btnPlay.Text = "PLAY\r\nS.P.R.!";
+            this.btnPlay.UseVisualStyleBackColor = true;
+            this.btnPlay.Click += new System.EventHandler(this.btnPlay_Click);
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Controls.Add(this.button1);
+            this.Controls.Add(this.btnPlay);
             this.Controls.Add(this.GBOne);
-            this.Controls.Add(this.pictureBox2);
-            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.img2);
+            this.Controls.Add(this.img1);
             this.Name = "Form1";
-            this.Text = "Form1";
-            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
-            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
+            this.Text = "Extreme S.P.R. Simuloator 2023";
+            ((System.ComponentModel.ISupportInitialize)(this.img1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.img2)).EndInit();
             this.GBOne.ResumeLayout(false);
             this.GBOne.PerformLayout();
             this.ResumeLayout(false);
@@ -138,13 +140,13 @@
 
         #endregion
 
-        private System.Windows.Forms.PictureBox pictureBox1;
-        private System.Windows.Forms.PictureBox pictureBox2;
+        private System.Windows.Forms.PictureBox img1;
+        private System.Windows.Forms.PictureBox img2;
         private System.Windows.Forms.RadioButton RBPaper;
         private System.Windows.Forms.RadioButton BRScissors;
         private System.Windows.Forms.RadioButton RBRock;
         private System.Windows.Forms.GroupBox GBOne;
-        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button btnPlay;
     }
 }
 
--- Form1.cs
+++ Form1.cs
@@ -12,32 +12,110 @@ namespace Part_6__PSR_project
 {
     public partial class Form1 : Form
     {
+
+        int userPlay = 0;
+        int computerPlay = 0;
+        int playerWin = 0;
+        int computerWin = 0;
+        int catsGame = 0;
+
+
         public Form1()
         {
             InitializeComponent();
 
-            
-        }
-        int userPlay = 0;
+            img1.Image = Properties.Resources.Openscene;
+            img2.Image = Properties.Resources.Openscene;
 
-       pictureBox1.Image = Properties.Resources.Rock;
-       pictureBox2.Image = Properties.Resources.Scissors;
+            
+
+
+
+
+
+
+
+
+        }
+
+        private void btnPlay_Click(object sender, EventArgs e)
+        {
+            if (userPlay == 1)
+            {
+                switch (computerPlay)
+                {
+                    case 1:
+                        catsGame++;
+                        break;
+
+                    case 2:
+                        computerWin++;
+                        break;
+
+                    case 3:
+                        playerWin++;
+                        break;
+                }
+
+            }
+
+            if (userPlay == 2)
+            {
+                switch (computerPlay)
+                {
+                    case 1:
+                        playerWin++;
+                        break;
+
+                    case 2:
+                        catsGame++;
+                        break;
+
+                    case 3:
+                        computerWin++;
+                        break;
+
+                }
+
+            }
+
+            if (userPlay == 3)
+            {
+                switch (computerPlay)
+                {
+                    case 1:
+                        computerWin++;
+                        break;
+
+                    case 2:
+                        playerWin++;
+                        break;
+
+                    case 3:
+                        catsGame++;
+                        break;
+
+                }
+
+            }
+        }
 
         //Use case statments with if statments inside to determine outcomes
-
         private void RBPaper_CheckedChanged(object sender, EventArgs e)
         {
-            userPlay
+            userPlay = 1;
         }
 
         private void BRScissors_CheckedChanged(object sender, EventArgs e)
         {
-
+            userPlay = 2;
         }
 
         private void RBRock_CheckedChanged(object sender, EventArgs e)
         {
-
+            userPlay = 3;
         }
+
+ 
     }
 }
