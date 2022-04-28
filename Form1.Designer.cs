3a3eb63d5660de9943e19d9865057ce2a6fe062d
Cole Palmer <PalmCole551@GoTVDSB.ca>
2022-04-28 10:45:22 -0400

    Done!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -35,6 +35,13 @@
             this.RBRock = new System.Windows.Forms.RadioButton();
             this.GBOne = new System.Windows.Forms.GroupBox();
             this.btnPlay = new System.Windows.Forms.Button();
+            this.lblComputerCounter = new System.Windows.Forms.Label();
+            this.lblPlayerCounter = new System.Windows.Forms.Label();
+            this.lblGameCounter = new System.Windows.Forms.Label();
+            this.btnReset = new System.Windows.Forms.Button();
+            this.lblComputerText = new System.Windows.Forms.Label();
+            this.lblUserText = new System.Windows.Forms.Label();
+            this.lblGameCount = new System.Windows.Forms.Label();
             ((System.ComponentModel.ISupportInitialize)(this.img1)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.img2)).BeginInit();
             this.GBOne.SuspendLayout();
@@ -42,6 +49,7 @@
             // 
             // img1
             // 
+            this.img1.BackColor = System.Drawing.Color.Transparent;
             this.img1.Location = new System.Drawing.Point(12, 12);
             this.img1.Name = "img1";
             this.img1.Size = new System.Drawing.Size(301, 278);
@@ -51,6 +59,7 @@
             // 
             // img2
             // 
+            this.img2.BackColor = System.Drawing.Color.Transparent;
             this.img2.Location = new System.Drawing.Point(487, 12);
             this.img2.Name = "img2";
             this.img2.Size = new System.Drawing.Size(301, 278);
@@ -110,24 +119,104 @@
             // 
             // btnPlay
             // 
+            this.btnPlay.BackColor = System.Drawing.Color.Lime;
             this.btnPlay.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.btnPlay.Location = new System.Drawing.Point(357, 212);
             this.btnPlay.Name = "btnPlay";
             this.btnPlay.Size = new System.Drawing.Size(90, 78);
             this.btnPlay.TabIndex = 6;
             this.btnPlay.Text = "PLAY\r\nS.P.R.!";
-            this.btnPlay.UseVisualStyleBackColor = true;
+            this.btnPlay.UseVisualStyleBackColor = false;
             this.btnPlay.Click += new System.EventHandler(this.btnPlay_Click);
+            // 
+            // lblComputerCounter
+            // 
+            this.lblComputerCounter.AutoSize = true;
+            this.lblComputerCounter.Font = new System.Drawing.Font("Microsoft Sans Serif", 16F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblComputerCounter.Location = new System.Drawing.Point(225, 343);
+            this.lblComputerCounter.Name = "lblComputerCounter";
+            this.lblComputerCounter.Size = new System.Drawing.Size(24, 26);
+            this.lblComputerCounter.TabIndex = 8;
+            this.lblComputerCounter.Text = "0";
+            // 
+            // lblPlayerCounter
+            // 
+            this.lblPlayerCounter.AutoSize = true;
+            this.lblPlayerCounter.Font = new System.Drawing.Font("Microsoft Sans Serif", 16F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblPlayerCounter.Location = new System.Drawing.Point(522, 343);
+            this.lblPlayerCounter.Name = "lblPlayerCounter";
+            this.lblPlayerCounter.Size = new System.Drawing.Size(24, 26);
+            this.lblPlayerCounter.TabIndex = 9;
+            this.lblPlayerCounter.Text = "0";
+            // 
+            // lblGameCounter
+            // 
+            this.lblGameCounter.AutoSize = true;
+            this.lblGameCounter.Font = new System.Drawing.Font("Microsoft Sans Serif", 16F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblGameCounter.Location = new System.Drawing.Point(390, 343);
+            this.lblGameCounter.Name = "lblGameCounter";
+            this.lblGameCounter.Size = new System.Drawing.Size(24, 26);
+            this.lblGameCounter.TabIndex = 10;
+            this.lblGameCounter.Text = "0";
+            // 
+            // btnReset
+            // 
+            this.btnReset.Location = new System.Drawing.Point(691, 415);
+            this.btnReset.Name = "btnReset";
+            this.btnReset.Size = new System.Drawing.Size(75, 23);
+            this.btnReset.TabIndex = 11;
+            this.btnReset.Text = "Reset";
+            this.btnReset.UseVisualStyleBackColor = true;
+            this.btnReset.Click += new System.EventHandler(this.btnReset_Click);
+            // 
+            // lblComputerText
+            // 
+            this.lblComputerText.AutoSize = true;
+            this.lblComputerText.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblComputerText.Location = new System.Drawing.Point(165, 296);
+            this.lblComputerText.Name = "lblComputerText";
+            this.lblComputerText.Size = new System.Drawing.Size(145, 24);
+            this.lblComputerText.TabIndex = 12;
+            this.lblComputerText.Text = "Computer score";
+            // 
+            // lblUserText
+            // 
+            this.lblUserText.AutoSize = true;
+            this.lblUserText.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblUserText.Location = new System.Drawing.Point(483, 296);
+            this.lblUserText.Name = "lblUserText";
+            this.lblUserText.Size = new System.Drawing.Size(105, 24);
+            this.lblUserText.TabIndex = 13;
+            this.lblUserText.Text = "Your Score";
+            // 
+            // lblGameCount
+            // 
+            this.lblGameCount.AutoSize = true;
+            this.lblGameCount.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblGameCount.Location = new System.Drawing.Point(336, 296);
+            this.lblGameCount.Name = "lblGameCount";
+            this.lblGameCount.Size = new System.Drawing.Size(130, 24);
+            this.lblGameCount.TabIndex = 14;
+            this.lblGameCount.Text = "Game counter";
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.Yellow;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.lblGameCount);
+            this.Controls.Add(this.lblUserText);
+            this.Controls.Add(this.lblComputerText);
+            this.Controls.Add(this.btnReset);
+            this.Controls.Add(this.lblGameCounter);
+            this.Controls.Add(this.lblPlayerCounter);
+            this.Controls.Add(this.lblComputerCounter);
             this.Controls.Add(this.btnPlay);
             this.Controls.Add(this.GBOne);
             this.Controls.Add(this.img2);
             this.Controls.Add(this.img1);
+            this.ForeColor = System.Drawing.Color.Black;
             this.Name = "Form1";
             this.Text = "Extreme S.P.R. Simuloator 2023";
             ((System.ComponentModel.ISupportInitialize)(this.img1)).EndInit();
@@ -135,6 +224,7 @@
             this.GBOne.ResumeLayout(false);
             this.GBOne.PerformLayout();
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
@@ -147,6 +237,13 @@
         private System.Windows.Forms.RadioButton RBRock;
         private System.Windows.Forms.GroupBox GBOne;
         private System.Windows.Forms.Button btnPlay;
+        private System.Windows.Forms.Label lblComputerCounter;
+        private System.Windows.Forms.Label lblPlayerCounter;
+        private System.Windows.Forms.Label lblGameCounter;
+        private System.Windows.Forms.Button btnReset;
+        private System.Windows.Forms.Label lblComputerText;
+        private System.Windows.Forms.Label lblUserText;
+        private System.Windows.Forms.Label lblGameCount;
     }
 }
 
--- Form1.cs
+++ Form1.cs
@@ -13,33 +13,33 @@ namespace Part_6__PSR_project
     public partial class Form1 : Form
     {
 
-        int userPlay = 0;
-        int computerPlay = 0;
-        int playerWin = 0;
-        int computerWin = 0;
-        int catsGame = 0;
+        //Random number generator
+        Random rnd = new Random();
 
-
+        //variables
+        int userPlay = 0;         //User Choice
+        int computerPlay = 0;     //Computer Choice
+        int playerWin = 0;        //Player Wins
+        int computerWin = 0;      //Computer Wins
+        int catsGame = 0;         //Tie Games
+        int gameCount = 0;        //Total Games
         public Form1()
         {
             InitializeComponent();
 
             img1.Image = Properties.Resources.Openscene;
             img2.Image = Properties.Resources.Openscene;
-
-            
-
-
-
-
-
-
-
-
         }
 
         private void btnPlay_Click(object sender, EventArgs e)
         {
+            //Generates computer play
+            computerPlay = rnd.Next(1, 4);
+
+            //Counts games
+            gameCount++;
+
+            //Checks user 1 against computer
             if (userPlay == 1)
             {
                 switch (computerPlay)
@@ -47,18 +47,16 @@ namespace Part_6__PSR_project
                     case 1:
                         catsGame++;
                         break;
-
                     case 2:
                         computerWin++;
                         break;
-
                     case 3:
                         playerWin++;
                         break;
                 }
-
             }
 
+            //Checks user 2 againts computer
             if (userPlay == 2)
             {
                 switch (computerPlay)
@@ -66,19 +64,16 @@ namespace Part_6__PSR_project
                     case 1:
                         playerWin++;
                         break;
-
                     case 2:
                         catsGame++;
                         break;
-
                     case 3:
                         computerWin++;
                         break;
-
                 }
-
             }
 
+            //Checks user 3 againts computer
             if (userPlay == 3)
             {
                 switch (computerPlay)
@@ -86,18 +81,47 @@ namespace Part_6__PSR_project
                     case 1:
                         computerWin++;
                         break;
-
                     case 2:
                         playerWin++;
                         break;
-
                     case 3:
                         catsGame++;
                         break;
-
                 }
-
             }
+            
+            //Prints images user
+            switch (userPlay)
+            {
+                case 1:
+                    img2.Image = Properties.Resources.Paper;
+                    break;
+                case 2:
+                    img2.Image = Properties.Resources.Scissors;
+                    break;
+                case 3:
+                    img2.Image = Properties.Resources.Rock;
+                    break;
+            }
+
+            //Prints images Computer
+            switch (computerPlay)
+            {
+                case 1:
+                    img1.Image = Properties.Resources.Paper;
+                    break;
+                case 2:
+                    img1.Image = Properties.Resources.Scissors;
+                    break;
+                case 3:
+                    img1.Image = Properties.Resources.Rock;
+                    break;
+            }
+
+            //Displays score
+            lblComputerCounter.Text = ($"{computerWin}");
+            lblPlayerCounter.Text = ($"{playerWin}");
+            lblGameCounter.Text = ($"{gameCount}");
         }
 
         //Use case statments with if statments inside to determine outcomes
@@ -105,17 +129,28 @@ namespace Part_6__PSR_project
         {
             userPlay = 1;
         }
-
         private void BRScissors_CheckedChanged(object sender, EventArgs e)
         {
             userPlay = 2;
         }
-
         private void RBRock_CheckedChanged(object sender, EventArgs e)
         {
             userPlay = 3;
         }
 
- 
+        private void btnReset_Click(object sender, EventArgs e)
+        {
+            img1.Image = Properties.Resources.Openscene; //reset first picture
+            img2.Image = Properties.Resources.Openscene; //reset second picture
+            //reset varibles
+            playerWin = 0;        //Player Wins
+            computerWin = 0;      //Computer Wins
+            catsGame = 0;         //Tie Games
+            gameCount = 0;        //Total Games
+            //Update screen
+            lblComputerCounter.Text = ($"{computerWin}");
+            lblPlayerCounter.Text = ($"{playerWin}");
+            lblGameCounter.Text = ($"{gameCount}");
+        }
     }
 }
